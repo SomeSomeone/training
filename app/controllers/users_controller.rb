e688bb1acee769ff0a6b3eb1149bb7cb722c130a
SomeSomeone <nice.gl_orl@ukr.net>
2015-02-28 07:57:58 +0200

    First User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+class UsersController < ApplicationController
+
+  def index
+    @users=User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+     @user = User.new(user_params)
+     respond_to do |format|
+      if @user.save
+        log_in @user
+        format.html { redirect_to @user, notice: 'Comment was successfully create.' }
+        format.json { render @user, status: :ok, location: @user }
+      else
+       format.html { render :new , alert: 'ALL BAD!' }
+       format.json { render json: @user, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+     respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'user was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def show
+    @user=User.find(params[:id])
+  end
+
+  def destroy
+    @user=User.find(params[:id])
+    if current_user?(@user)
+      log_out
+    end
+    @user.destroy
+    redirect_to root_url
+
+  end
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation, :avatar)
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,56 @@
+class User < ActiveRecord::Base
+
+	attr_accessor :remember_token
+	validates :name ,presence: true,	length: {maximum:50}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true,	length: {maximum:255},
+										format:{with: VALID_EMAIL_REGEX},
+										uniqueness: true
+	validates :password, length: {minimum:6}
+	has_secure_password
+	before_save :downcase_email
+
+
+	has_attached_file :avatar, :styles => { :standart => "300x300>", :small => "50x50#" }, :default_url => "/images/:style/missing.png"
+	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
+#	validates_attachment :avatar,
+#	:content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"] }
+
+
+	#для запоминающей галочки
+	def remember
+		self.remember_token=User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+
+	def forget
+		update_attribute(:remember_digest,nil)
+	end
+	
+	# для зашифровки (типа как пароль)
+	def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+	end
+	#генерация случайного набора
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	#как я понимаю вещь для сверки верности заколировки тем самым захода через печенье
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	end
+
+	private
+
+	def downcase_email
+		self.email.downcase!
+	end
+end
+
